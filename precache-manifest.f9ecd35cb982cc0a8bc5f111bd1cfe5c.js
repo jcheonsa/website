76c8790a1fb184362eef55dfa0b0c1ffc6da718e
Johnnie Tan <73861050+jcheonsa@users.noreply.github.com>
2021-05-14 19:13:49 -0700

    Updates

---
+++ precache-manifest.f9ecd35cb982cc0a8bc5f111bd1cfe5c.js
@@ -0,0 +1,166 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "12ad9b06b46ba20a8b56e14668de9b7b",
+    "url": "/website/index.html"
+  },
+  {
+    "revision": "f6c1e7fd0b7aaa587717",
+    "url": "/website/static/css/main.85cea0ce.chunk.css"
+  },
+  {
+    "revision": "7dbca81b36fac5d725c7",
+    "url": "/website/static/js/2.cacb6222.chunk.js"
+  },
+  {
+    "revision": "7697640d703ae35e4d2120f28e9ccbdd",
+    "url": "/website/static/js/2.cacb6222.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f6c1e7fd0b7aaa587717",
+    "url": "/website/static/js/main.f8814156.chunk.js"
+  },
+  {
+    "revision": "3ca2c82da3288b4209bc",
+    "url": "/website/static/js/runtime-main.52a8e150.js"
+  },
+  {
+    "revision": "8702dc53f2650b6cddd504315005efda",
+    "url": "/website/static/media/aboutme.8702dc53.svg"
+  },
+  {
+    "revision": "e7b9c2d4f866a0540be8aa10e83f7f26",
+    "url": "/website/static/media/art.e7b9c2d4.svg"
+  },
+  {
+    "revision": "3c682f41c1f16ee65574ec6fc7ba85ee",
+    "url": "/website/static/media/autism.3c682f41.jpg"
+  },
+  {
+    "revision": "f87856ff376d1292e5042d36107f9169",
+    "url": "/website/static/media/background.f87856ff.png"
+  },
+  {
+    "revision": "babe1f2005f5e4f6d3a4e066ede42867",
+    "url": "/website/static/media/bbkp.babe1f20.png"
+  },
+  {
+    "revision": "65aa02addcd6b6add2292f96d134b84e",
+    "url": "/website/static/media/bluebear.65aa02ad.webp"
+  },
+  {
+    "revision": "194435dcd8d41e0e0ef36ebdae817b4e",
+    "url": "/website/static/media/contactme.194435dc.svg"
+  },
+  {
+    "revision": "7265f9b66ab9c32e1ea08d840df223fa",
+    "url": "/website/static/media/copythat.7265f9b6.jpg"
+  },
+  {
+    "revision": "94c57ca3757832d2cbf6296f36016b4b",
+    "url": "/website/static/media/daigo1.94c57ca3.png"
+  },
+  {
+    "revision": "f759a8e553c8087b9b6b04f5e20f9057",
+    "url": "/website/static/media/discord.f759a8e5.svg"
+  },
+  {
+    "revision": "c170f3b43587226a6e74ea704183d2a2",
+    "url": "/website/static/media/down-arrow.c170f3b4.svg"
+  },
+  {
+    "revision": "7a2e7622362e74fc2bb93cabb8da32fb",
+    "url": "/website/static/media/email.7a2e7622.svg"
+  },
+  {
+    "revision": "ec5bc1b3148ca464a3a3e17af89c3ead",
+    "url": "/website/static/media/github.ec5bc1b3.svg"
+  },
+  {
+    "revision": "648c4ff8cc4e8bfeee6d42dd85145beb",
+    "url": "/website/static/media/home.648c4ff8.svg"
+  },
+  {
+    "revision": "596caf145715cc08f128b934c2316400",
+    "url": "/website/static/media/ieee.596caf14.png"
+  },
+  {
+    "revision": "668fa3da1663f5e316c97259fdb994e3",
+    "url": "/website/static/media/insta-UI.668fa3da.png"
+  },
+  {
+    "revision": "dd1b1fa2a845379ba2fee663029553f6",
+    "url": "/website/static/media/insta-UI2.dd1b1fa2.png"
+  },
+  {
+    "revision": "aaf159630bd38a84032d9b872ba4a0fa",
+    "url": "/website/static/media/johnnie.aaf15963.jpg"
+  },
+  {
+    "revision": "28229bd3374ef4b9df21d53baeb7d5f2",
+    "url": "/website/static/media/jtanlogo.28229bd3.png"
+  },
+  {
+    "revision": "6496b57a39f8fcfb68ec6714fe41648f",
+    "url": "/website/static/media/jtanlogo.6496b57a.gif"
+  },
+  {
+    "revision": "75a317c7d93197730ef157eaa34ff79b",
+    "url": "/website/static/media/kaibabot.75a317c7.png"
+  },
+  {
+    "revision": "07a9c904affb6727c189468a7a0f71a8",
+    "url": "/website/static/media/ketchupo.07a9c904.webp"
+  },
+  {
+    "revision": "1e01f0c0dea217ba568ebbcec49c5b6b",
+    "url": "/website/static/media/linkedin.1e01f0c0.svg"
+  },
+  {
+    "revision": "7d59f1be6d70ac216b7fa3625795c2ec",
+    "url": "/website/static/media/lisabot.7d59f1be.png"
+  },
+  {
+    "revision": "29e6e00102fa1c0393512477ddfb5c9f",
+    "url": "/website/static/media/loveit2.29e6e001.png"
+  },
+  {
+    "revision": "eb86a5d95a894ee05647acd995b852ba",
+    "url": "/website/static/media/pfp3.eb86a5d9.jpg"
+  },
+  {
+    "revision": "aaa05f30875803adb130c6066e4174bc",
+    "url": "/website/static/media/portfolio.aaa05f30.svg"
+  },
+  {
+    "revision": "cb80ef2f4963a63f8ddb40a538d3e3c8",
+    "url": "/website/static/media/rosebot.cb80ef2f.png"
+  },
+  {
+    "revision": "5174c736af02c5d3d4badc60c49cf5ba",
+    "url": "/website/static/media/scuffed.5174c736.png"
+  },
+  {
+    "revision": "2f53e079aabe1be0076778923dd9bd0c",
+    "url": "/website/static/media/searchstep1.2f53e079.png"
+  },
+  {
+    "revision": "28486453d932c6b80b4f14e41b77f6b4",
+    "url": "/website/static/media/searchstep2.28486453.png"
+  },
+  {
+    "revision": "eaffbac4d8479c5e08ee50f2adc126d3",
+    "url": "/website/static/media/spotify-UI.eaffbac4.png"
+  },
+  {
+    "revision": "fd4e8664dba92f2caae95335d44709fa",
+    "url": "/website/static/media/tab.fd4e8664.svg"
+  },
+  {
+    "revision": "e5e62321891d8f1145f9de716a75b60a",
+    "url": "/website/static/media/x.e5e62321.svg"
+  },
+  {
+    "revision": "54cdb8c19f607fbf084839be3b4e1be8",
+    "url": "/website/static/media/zucker.54cdb8c1.webp"
+  }
+]);
